aa057f93f6f10df2146b0734a5d64838022cce5f
Paweł Morawczyński <burciak01@gmail.com>
2024-12-05 00:08:28 +0100

    fix: reset password link fixed

--- FitUpAppBackend.Application/Identity/Commands/ResetPasswordRequest/ResetPasswordRequestHandler.cs
+++ FitUpAppBackend.Application/Identity/Commands/ResetPasswordRequest/ResetPasswordRequestHandler.cs
@@ -1,8 +1,7 @@
-using FitUpAppBackend.Core.Identity.Entities;
 using FitUpAppBackend.Core.Identity.Services;
 using FitUpAppBackend.Core.Integrations.Email.Services;
+using FitUpAppBackend.Core.Integrations.Frontend.Configurations;
 using FitUpAppBackend.Shared.Abstractions.Commands;
-using Microsoft.AspNetCore.Identity;
 
 namespace FitUpAppBackend.Application.Identity.Commands.ResetPasswordRequest;
 
@@ -10,15 +9,16 @@ public sealed class ResetPasswordRequestHandler : ICommandHandler<ResetPasswordR
 {
     private readonly IIdentityService _identityService;
     private readonly IEmailService _emailService;
+    private readonly FrontendConfig _frontendConfig;
 
     private readonly string _resetPasswordTemplateId = "d-2e01bae7bcf7497a821c74528552c406";
     private readonly string _resetPasswordSubject = "Fit UP change password request";
 
-    public ResetPasswordRequestHandler(IIdentityService identityService, UserManager<User> userManager,
-        IEmailService emailService)
+    public ResetPasswordRequestHandler(IIdentityService identityService, IEmailService emailService, FrontendConfig frontendConfig)
     {
         _identityService = identityService;
         _emailService = emailService;
+        _frontendConfig = frontendConfig;
     }
 
     public async Task HandleAsync(ResetPasswordRequestCommand command, CancellationToken cancellationToken)
@@ -28,7 +28,7 @@ public sealed class ResetPasswordRequestHandler : ICommandHandler<ResetPasswordR
         var email = command.Email;
         var token = passwordResetToken;
         
-        var linkToResetPassword = $"http://localhost:3000/reset-password?{nameof(token)}={token}&{nameof(email)}={email}";
+        var linkToResetPassword = $"{_frontendConfig.BaseUrl}/reset-password?{nameof(token)}={token}&{nameof(email)}={email}";
 
         var templateData = new { url = linkToResetPassword };
 
